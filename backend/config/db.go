898bdb8eda8196082a4f6ee8c4b8bc90b6714d87
Tawunchai Burakhon <161191262+Tawunchai@users.noreply.github.com>
2026-01-14 12:55:32 +0700

    add

--- backend/config/db.go
+++ backend/config/db.go
@@ -431,7 +431,7 @@ func seedContent(db *gorm.DB) {
 		Status:       "Active",
 		Latitude:     14.8802,
 		Longitude:    102.018,
-		StopPolicy:   70,
+		StopPolicy:   3.75,
 		Image:        "uploads/cabinet/cabinet.jpg",
 		UrlWebsocket: "wss://api.evstation-sut.it.com/ocpp/",
 		ChargePoint:  "CP_1",
